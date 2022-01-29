d38e45414d47dbcb8f821fc1fe541181f8ae1e37
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2022-01-29 18:18:48 -0300

    first architecture propose

---
+++ src/components/Instructions/CardsContainer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <card-left/>
+        <card-right/>
+    </div>
+</template>
+
+<script>
+import CardLeft from "./CardLeft.vue";
+import CardRight from "./CardRight.vue";
+
+export default {
+    components:{
+        CardLeft,
+        CardRight,
+    }
+}
+</script>
+
+<style>
+
+</style>

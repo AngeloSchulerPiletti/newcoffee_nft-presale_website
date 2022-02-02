1d78e1a85073404d4a92df1b80cae60b563d4389
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2022-02-02 13:36:42 -0300

    roadmap component created

---
+++ src/components/RoadMap/RoadMapContainer.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container grid">
+    <div class="line flex_c">
+      <road-map-item
+        v-for="(item, index) in getRoadMapData"
+        :key="index"
+        :data="item"
+      />
+    </div>
+    <img id="cake" class="foods" src="@/assets/images/game-itens/Food1.png"/>
+    <img id="pastel" class="foods" src="@/assets/images/game-itens/Food2.png"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import RoadMapItem from "@/components/RoadMap/RoadMapItem.vue";
+
+export interface RoadMapInterface {
+  year: number;
+  month: string;
+  title?: string;
+  items?: [string];
+}
+
+export default defineComponent({
+  computed: {
+    getRoadMapData(): [RoadMapInterface] {
+      return require("@/data/rodemap.json");
+    },
+  },
+  components: {
+    RoadMapItem,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  grid-template-columns: 7fr 3fr;
+  padding-left: 200px;
+  margin: 200px 0 150px 0;
+  position: relative;
+
+  .line {
+    padding: 40px 0;
+    border-left: 6px solid #000;
+    position: relative;
+    gap: 20px;
+  }
+  #cake {
+    right: 0;
+    top: 50%;
+    width: 30vw;
+    height: 30vw;
+    transform: translate(40%, -50%) rotate(30deg);
+  }
+  #pastel{
+    left: 0;
+    width: 35vw;
+    height: 35vw;
+    transform: translateX(-50%) rotate(70deg);
+  }
+}
+</style>
---
+++ src/components/RoadMap/RoadMapItem.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="item-container flex_c">
+    <h5 class="date">
+      {{ `${data.month}, ${data.year}` }}
+    </h5>
+    <div v-if="data.title && data.items" class="content">
+      <h6 class="float-title">{{ data.title }}</h6>
+      <ul class="flex_c">
+        <li v-for="(event, index) in data.items" :key="index">{{ event }}</li>
+      </ul>
+    </div>
+    <h6 v-else class="blocked-title"></h6>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { PropType } from "@vue/runtime-core";
+import { RoadMapInterface } from "@/components/RoadMap/RoadMapContainer.vue";
+
+export default defineComponent({
+  props: {
+    data: {
+      type: Object as PropType<RoadMapInterface>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.item-container {
+  transform: translateX(-20px);
+  .date {
+    position: relative;
+    width: fit-content;
+    padding: 8px 18px;
+    font-size: 48px;
+    outline: 3px solid #000;
+    background-color: $bege-fraco;
+    box-shadow: 3px 3px 8px #00000080;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      outline: 3px solid #000;
+      background-color: #00000020;
+      transform: translate(8px, 8px);
+      z-index: -1;
+    }
+  }
+  .content {
+    border: 3px solid #000;
+    width: 500px;
+    padding: 38px 0 14px 15px;
+    margin-left: 200px;
+    transform: translateY(-20px);
+    position: relative;
+    z-index: -1;
+    background-color: $bege-fraco;
+    box-shadow: 3px 3px 8px #00000070;
+
+    .float-title {
+      position: absolute;
+      background-color: #000;
+      color: $white;
+      font-size: 19px;
+      padding: 8px 12px;
+      top: 0;
+      right: 0;
+      border-bottom-left-radius: 6px;
+    }
+    ul {
+      font-size: 20px;
+      gap: 5px;
+      font-weight: 300;
+    }
+  }
+    .blocked-title {
+    }
+}
+</style>

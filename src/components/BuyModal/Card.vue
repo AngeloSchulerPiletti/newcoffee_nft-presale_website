d218d9f6ed78135333a0c44e992ca5765a96a034
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2022-02-03 22:48:47 -0300

    Modal's Bau Card created

--- src/components/BuyModal/Card.vue
+++ src/components/BuyModal/Card.vue
@@ -1,13 +1,28 @@
 <template>
-  
+  <div class="container">
+    <div class="card-bau">
+      <img :src="`/images/game-images/${imageName}`" alt="Baú do jogo" />
+    </div>
+    <span class="price"> U${{ price }} </span>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    price: String,
+    imageName: String,
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.container {
+  cursor: pointer;
 
+  .card-bau {
+    img {
+    }
+  }
+}
 </style>

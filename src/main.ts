9366edd192be39e5523b9b497221452ae9404ae9
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2022-02-03 21:57:59 -0300

    adding ethereum property to global window

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,10 @@ import router from "./router";
 import { store } from "./store";
 import "@/assets/css/index.css"
 
+declare global {
+    interface Window {
+        ethereum:any;
+    }
+}
+
 createApp(App).use(store).use(router).mount("#app");

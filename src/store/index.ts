f66ceccada4fc948cefa5c61d659a46ce128e349
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2022-02-03 21:59:18 -0300

    wallet properties and settings added

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,13 +2,23 @@ import { createStore } from 'vuex'
 
 export type State = {
   modalBoolState: boolean,
+  hasMetaMask: boolean,
+  walletAddress: string | null,
 }
 
 export const store = createStore({
   state: {
     modalBoolState: false,
+    hasMetaMask: false,
+    walletAddress: null,
   },
   mutations: {
+    setWalletAddress(state: State, address: string){
+      state.walletAddress = address;
+    },
+    setHasMetaMask(state: State, payload: boolean){
+      state.hasMetaMask = payload;
+    },
     openModal(state: State){
       state.modalBoolState = true;
     },
